b9e140d8f7af1e9b890766bbfa3f3997e553ec45
justintemps <jesmith627@gmail.com>
2016-09-11 23:36:14 +0200

    Make header pretty sort of.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,22 +7,19 @@ const Header = React.createClass({
 			<header id="banner" role="banner">
 				<div id="primary-banner">
 					<div className="container">
-						<div className="row">
+						<div className="header-row">
 							<div className="large-6-col">
-								<a id="logo" className="navbar-brand" href="/global/lang--en/index.htm">
-									<h1>ILO</h1>
-								</a>
+								<a id="logo" className="navbar-brand" href="/global/lang--en/index.htm"></a>
 							</div>
 							<div className="large-6-col">
 								<h2>Promoting Jobs, Protecting People</h2>
-								<h3>ILO is a specialised agency of <a href="http://www.un.org/en/aboutun/structure/">the United Nations</a></h3>
+								<h3>ILO is a specialised agency of the <a href="http://www.un.org/en/aboutun/structure/">United Nations</a></h3>
+									<div className="language">
+										<a href="/global/lang--fr/index.htm">français</a>
+										<a href="/global/lang--es/index.htm">español</a>
+									</div>
 							</div>
-							<div className="language">
-								<a href="/global/lang--fr/index.htm">français</a>
-							</div>
-							<div>
-								<a href="/global/lang--es/index.htm">español</a>
-							</div>
+
 						</div>
 					</div>
 				</div>
