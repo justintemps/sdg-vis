55f74a732cdc53cdb2bbd57c115d4e848d8fe6f0
justintemps <jesmith627@gmail.com>
2016-09-16 12:15:35 +0200

    Add email signup form. Remove other left column business

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -22,41 +22,25 @@ const Sidebar = React.createClass({
 					</li>
 				</ol>
 				<div className="furniture feature-multiple slim-bar">
-					<h4>Further information</h4>
-					<ol className="item-list">
-						<li>
-							<a target="" href="/global/topics/decent-work/lang--en/index.htm">ILO portal on Decent Work&nbsp;<span className="linked fa fa-angle-right"></span></a>
-						</li>
-					</ol>
-					<h5>Contact us</h5>
-					<ol className="item-list">
-						<li>
-							<a target="" href="mailto:communication@ilo.org">communication@ilo.org&nbsp;<span className="linked fa fa-angle-right"></span></a>
-						</li>
-					</ol>
-					<h5>U.N. Knowledge platform on sustainable development</h5>
-					<ol className="item-list">
-						<li>
-							<a href="https://sustainabledevelopment.un.org/" target="">sustainabledevelopment.un.org&nbsp;<span className="linked fa fa-angle-right"></span></a>
-						</li>
-					</ol>
-				</div>
-				<div className="furniture feature-multiple text-wrap text-wrap-img-left no-img-scale standard-bar">
-					<ol className="item-list ">
-						<li className="highlight">
-							<a target="" href="/global/topics/sdg-2030/resources/WCMS_436923/lang--en/index.htm" className="item-image">
-								<img src="http://www.ilo.org/wcmsp5/groups/public/---dgreports/---dcomm/documents/image/wcms_486800.jpg" alt="" />
-							</a>
-							<span className="caption"></span>
-							<p className="item-prehead">Brochure</p>
-							<h5 className="item-title">
-								<a target="" href="/global/topics/sdg-2030/resources/WCMS_436923/lang--en/index.htm">Decent work, a key to the 2030 agenda for sustainable development&nbsp;<span className="linked fa fa-angle-right"></span>
-								</a>
-							</h5>
-							<p className="item-info">‎pdf -&nbsp;&nbsp;2.1 MB‎</p>
-						</li>
-					</ol>
-				</div>
+					<h4>Receive ILO news<span className="fa fa-envelope"></span></h4>
+						<div className="freeform text">
+							<form acceptCharset="UTF-8" method="post" action="http://ilo.msgfocus.com/k/Ilo/news_updates">
+								<p>Send us your email and get ILO news delivered weekly straight to your mailbox</p>
+								<div className="input-container">
+									<div id="amf-el-email_37" className="amf-el amf-type-textbox">
+										<div className="amf-el-inner">
+											<input type="text" maxLength="255" id="amf-input-email_37" name="email_37" className="search" />
+										</div>
+									</div>
+									<input type="hidden" value="form_website" id="amf-hidden-lead_source_48" name="lead_source_48" />
+									<input type="hidden" value="en" id="amf-hidden-language_49" name="language_49" />
+									<div id="amf-el-submit" className="amf-el amf-type-button">
+										<input type="submit" value="Submit" id="submit" name="submit" className="butt" />
+									</div>
+								</div>
+							</form>
+						</div>
+					</div>
 			</div>
 		);
 	}
