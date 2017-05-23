fce75c0637498f778d7e23c6a6e81dae5968d9b4
justintemps <jesmith627@gmail.com>
2017-05-23 17:23:53 +0200

    Figure out fullscreen!

--- src/Components/ViewerWindow/ViewerWindow.jsx
+++ src/Components/ViewerWindow/ViewerWindow.jsx
@@ -3,51 +3,53 @@
  * Parent to TitleBar, ViewerWindowContent
  */
 
-import React from "react";
-import TitleBar from "../TitleBar/TitleBar.jsx";
-import ViewerWindowContent from "../ViewerWindowContent/ViewerWindowContent.jsx";
-import Modal from "../Modal/Modal.jsx";
-
+import React from 'react';
+import TitleBar from '../TitleBar/TitleBar.jsx';
+import ViewerWindowContent
+  from '../ViewerWindowContent/ViewerWindowContent.jsx';
+import Modal from '../Modal/Modal.jsx';
 
 const ViewerWindow = React.createClass({
-
-	mountModal() {
-		if (this.props.isModalOpen) {
-			return(
-				<Modal closeModal={this.props.closeModal}>
-					<div className="iframe-wrapper">
-						<iframe
-							src="https://www.youtube.com/embed/mZpyJwevPqc?rel=0&amp;controls=1&amp;showinfo=0&amp;autoplay=1&amp;modestbranding=1"
-							frameBorder="0"
-						/>
-					</div>
-				</Modal>
-			);
-		}
-	},
-
-  requestFullscreen() {
-    const body = document.getElementsByTagName('body');
-    body.requestFullscreen();
+  mountModal() {
+    if (this.props.isModalOpen) {
+      return (
+        <Modal closeModal={this.props.closeModal}>
+          <div className="iframe-wrapper">
+            <iframe
+              src="https://www.youtube.com/embed/mZpyJwevPqc?rel=0&amp;controls=1&amp;showinfo=0&amp;autoplay=1&amp;modestbranding=1"
+              frameBorder="0"
+            />
+          </div>
+        </Modal>
+      );
+    }
   },
 
-	render() {
-		const titleBar = <TitleBar
-			sdgNumber={this.props.data[this.props.currentSdg].sdgNumber}
-			sdgName={this.props.data[this.props.currentSdg].sdgName}
-			sdgColor= {this.props.data[this.props.currentSdg].sdgColor}
-			getVPwidth={this.props.getVPwidth}
-		/>;
+  requestFullscreen(e) {
+    document.documentElement.webkitRequestFullscreen();
+  },
 
-		return (
-			<div className="viewerWindow">
-				{titleBar}
-				<ViewerWindowContent {...this.props} />
-				{this.mountModal()}
-        <button onClick={this.requestFullscreen}>Click me for full screen!</button>
-			</div>
-		);
-	}
+  render() {
+    const titleBar = (
+      <TitleBar
+        sdgNumber={this.props.data[this.props.currentSdg].sdgNumber}
+        sdgName={this.props.data[this.props.currentSdg].sdgName}
+        sdgColor={this.props.data[this.props.currentSdg].sdgColor}
+        getVPwidth={this.props.getVPwidth}
+      />
+    );
+
+    return (
+      <div className="viewerWindow">
+        {titleBar}
+        <ViewerWindowContent {...this.props} />
+        {this.mountModal()}
+        <button onClick={this.requestFullscreen}>
+          Click me for full screen!
+        </button>
+      </div>
+    );
+  }
 });
 
 export default ViewerWindow;
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import {render} from "react-dom";
+import React from 'react';
+import { render } from 'react-dom';
 
-import "./styles/main.scss";
-import "./fonts/fonts.scss";
-import "./fonts/font-awesome-4.6.3/scss/font-awesome.scss";
+import './styles/main.scss';
+import './fonts/fonts.scss';
+import './fonts/font-awesome-4.6.3/scss/font-awesome.scss';
 
-import SDGexplorer from "./Components/SDGexplorer/SDGexplorer.jsx";
+import SDGexplorer from './Components/SDGexplorer/SDGexplorer.jsx';
 
-render(<SDGexplorer />, document.getElementById("app"));
+render(<SDGexplorer />, document.getElementById('app'));
