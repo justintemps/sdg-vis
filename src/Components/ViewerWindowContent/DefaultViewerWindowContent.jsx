3d36af9ec52917d7c57279787daae5b1b68a4082
justintemps <jesmith627@gmail.com>
2017-05-30 18:05:37 +0200

    Tweaks

--- src/Components/ViewerWindowContent/DefaultViewerWindowContent.jsx
+++ src/Components/ViewerWindowContent/DefaultViewerWindowContent.jsx
@@ -4,9 +4,9 @@ function DefaultViewerWindowContent(props) {
   return (
     <div className="defaultViewerWindowContent">
       <div>
-        <h1>Touch the icons</h1>
+        <h1>Touch the goals</h1>
         <p>
-          to learn how the ILO is working to acheive the Sustainable Deverlopment Goals
+          to learn how the ILO is working to achieve the 2030 Sustainable Development Agenda
         </p>
       </div>
       <div>
