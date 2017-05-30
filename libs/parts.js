ddebf79a98ce897b018bbc261d2410c08586a110
justintemps <jesmith627@gmail.com>
2017-05-30 22:25:02 +0200

    Add data

--- libs/parts.js
+++ libs/parts.js
@@ -1,19 +1,19 @@
-const webpack = require("webpack");
+const webpack = require('webpack');
 
 exports.devServer = function(options) {
-	return {
-		devServer: {
-//			historyApiFallback: true
-			hot: true,
-			inline: true,
-			stats: "errors-only",
-			host: options.host,
-			port: options.port
-		},
-		plugins: [
-			new webpack.HotModuleReplacementPlugin({
-				multiStep: true
-			})
-		]
-	};
+  return {
+    devServer: {
+      //			historyApiFallback: true
+      hot: true,
+      inline: true,
+      stats: 'errors-only',
+      host: options.host,
+      port: options.port
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin({
+        multiStep: true
+      })
+    ]
+  };
 };
--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,9 @@ import { render } from 'react-dom';
 import './styles/main.scss';
 import './fonts/fonts.scss';
 import './fonts/font-awesome-4.6.3/scss/font-awesome.scss';
-
+import getData from '../libs/getData.js';
 import SDGexplorer from './Components/SDGexplorer/SDGexplorer.jsx';
+
+getData();
 
 render(<SDGexplorer />, document.getElementById('app'));
