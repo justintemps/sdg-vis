b8c6ef6ecf4d38b9215ab32c330f7246f1833a34
justintemps <jesmith672@gmail.com>
2017-12-18 11:02:32 +0100

    Fix story rendering

--- src/Components/ImpactStory/ImpactStory.jsx
+++ src/Components/ImpactStory/ImpactStory.jsx
@@ -3,6 +3,19 @@ import ShareWidget from '../ShareWidget/ShareWidget.jsx';
 import Arrow from '../Arrow/Arrow.jsx';
 
 const ImpactStory = React.createClass({
+
+
+  shortVersion(blurb, limit) {
+      if (blurb.length < limit) {
+          return blurb;
+      } else {
+         let shortBlurb = blurb.slice(0, limit).split(' ');
+         shortBlurb.pop();
+         return `${shortBlurb.join(' ')} . . .`;
+      }
+  },
+
+
   render() {
     return (
       <div className="stories-wrapper">
@@ -12,9 +25,9 @@ const ImpactStory = React.createClass({
               href={this.props.url}
               target="_blank"
             >
-              <h2>{this.props.title}</h2>
+              <h2>{this.shortVersion(this.props.title, 100)}</h2>
             </a>
-            <p>{this.props.blurb}</p>
+            <p>{this.shortVersion(this.props.blurb, 250)}</p>
             <ShareWidget
               currentStoryUrl={
                 this.props.url
